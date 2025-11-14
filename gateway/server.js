8e86726ff03b25453c7980e05570eb3d485439f9
Amish <amishcys@gmail.com>
2025-11-14 22:49:40 +0500

    something

--- gateway/server.js
+++ gateway/server.js
@@ -6,7 +6,7 @@ import { createProxyMiddleware } from 'http-proxy-middleware';
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+// const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors());
